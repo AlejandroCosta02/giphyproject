4c9deda9a30fad22a3ed1433684a5f2bc270caae
Alejandro Costa <costa.claudio.alejandro@gmail.com>
2023-02-27 13:06:54 -0300

    footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <div className="max-w-2xl mx-auto text-white py-10">
         <div className="text-center">
             <h3 className="text-3xl mb-3">Lorem ipsum dolor sit amet.</h3>
-            <p> Stay fit. All day, every day. </p>
+            <p>Lorem ipsum dolor sit.</p>
             <div className="flex justify-center my-10">
                 <div className="flex items-center border rounded-lg px-4 py-2 w-52 mx-2">
                     <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" className="w-7 md:w-8" />
@@ -25,7 +25,7 @@ function Footer() {
             </div>
         </div>
         <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
-            <p className="order-2 md:order-1 mt-8 md:mt-0"> &copy; Beautiful Footer, 2021. </p>
+            <p className="order-2 md:order-1 mt-8 md:mt-0"> &copy; Costa Alejandro. 2023. </p>
             <div className="order-1 md:order-2">
                 <span className="px-2">About us</span>
                 <span className="px-2 border-l">Contact us</span>
